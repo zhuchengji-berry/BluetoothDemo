f0a31e311c983fd5d2186cae26cb2351aa330b0e
chenji <83136645@qq.com>
2020-10-28 16:06:23 +0800

    Initial Commit

---
+++ BluetoothDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BluetoothDemo
+//
+//  Created by 沉寂 on 2020/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
